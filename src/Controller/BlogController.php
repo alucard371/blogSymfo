b46328f86e3168845a30cd3322194bb303f2e711
alucard371 <test8q@gmail.com>
2018-08-28 12:03:07 +0200

    showing blog post

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,18 +2,41 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction()
-    {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
-        ]);
-    }
+
+	/** @var EntityManagerInterface */
+	private $entityManager;
+
+	/** @var \Doctrine\Common\Persistence\ObjectRepository */
+	private $authorRepository;
+
+	/** @var \Doctrine\Common\Persistence\ObjectRepository */
+	private $blogPostRepository;
+
+	/**
+	 * @param EntityManagerInterface $entityManager
+	 */
+	public function __construct(EntityManagerInterface $entityManager)
+	{
+		$this->entityManager = $entityManager;
+		$this->blogPostRepository = $entityManager->getRepository('App:BlogPost');
+		$this->authorRepository = $entityManager->getRepository('App:Author');
+	}
+
+	/**
+	 * @Route("/", name="homepage")
+	 * @Route("/entries", name="entries")
+	 */
+	public function entriesAction()
+	{
+		return $this->render('blog/entries.html.twig', [
+			'blogPosts' => $this->blogPostRepository->findAll()
+		]);
+	}
 }
